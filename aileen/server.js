9356ce116eed7e2b34fefdc518cdd48d1c5f5b6a
Aileen Wong <aileen.m.wong@gmail.com>
2017-09-19 16:40:43 -0400

    added db folder

---
+++ aileen/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+
+
+app.listen(port,() => {
+  console.log(`Listening on port ${port}`);
+})
+
+
+app.get('/', function (req, res) {
+  res.send('This is the main page');
+});
