502b5e222aa75f75d97eab53f1323290b9421dc9
Aileen Wong <aileen.m.wong@gmail.com>
2017-09-20 09:16:07 -0400

    add scaffolding for express

--- aileen/server.js
+++ aileen/server.js
@@ -1,14 +1,30 @@
 const express = require('express');
+const path = require('path');
+const logger = require('morgan');
+const bodyParser = require('body-parser');
 const app = express();
-const port = 3000;
 
+app.use('/static', express.static(path.join(__dirname, 'public')));
 
+app.use(logger('dev'));
 
-app.listen(port,() => {
-  console.log(`Listening on port ${port}`);
-})
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
 
+// app.get('/', function(req, res) {
+//   res.sendFile(__dirname + '/public/index.html');
+// });
 
-app.get('/', function (req, res) {
-  res.send('This is the main page');
+// /* tweeds API route */
+// const tweedRoutes = require('./routes/tweedroutes');
+// app.use('/api/tweedr', tweedRoutes);
+
+/* handling 404 */
+app.get('*', function(req, res) {
+  res.status(404).send({message: 'Oops! Not found.'});
+});
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, function() {
+  console.log(`listening on port ${PORT}`);
 });
