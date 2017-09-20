b43ad3f20b3e5ad431dce993bb1dfd2a2be019c0
mao hung <maohung71@gmail.com>
2017-09-20 17:04:36 -0400

    cloudinary api working

--- outfit/client/src/App.js
+++ outfit/client/src/App.js
@@ -1,5 +1,9 @@
 import React, { Component } from 'react';
 import { Navbar, Jumbotron, Button } from 'react-bootstrap';
+import { Image } from 'cloudinary-react';
+import axios from 'axios';
+
+
 import './App.css';
 
 import OutfitEdit from './components/OutfitEdit';
@@ -10,6 +14,68 @@ import Header from './components/Header';
 import { Route, Redirect, Switch } from 'react-router-dom';
 
 class App extends Component {
+  constructor(){
+    super();
+
+    this.state = {
+      image: null,
+      imgPreview: null
+    }
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(e){
+    if (e.currentTarget.files.length === 1) {
+      const fReader = new FileReader();
+      fReader.addEventListener(
+        'load',
+        () => {
+          // rendering file preview
+          this.setState({  image: fReader.result });
+        },
+        false
+      );
+
+      if (e.currentTarget.files['0']) {
+        // reading file
+        fReader.readAsDataURL(e.currentTarget.files['0']);
+      }
+      return;
+    }
+  }
+
+  handleSubmit(e){
+    e.preventDefault();
+
+    var CLOUDINARY_URL = 'https://api.cloudinary.com/v1_1/ga-mao/image/upload';
+    var CLOUDINARY_UPLOAD_PRESET = 'jd8p5ppp';
+
+    var file = this.state.image;
+    var formData = new FormData();
+    formData.append('file', file);
+    formData.append('upload_preset', CLOUDINARY_UPLOAD_PRESET);
+
+    axios({
+      url: CLOUDINARY_URL,
+      method: "POST",
+      headers: {
+        'Content-Type' : 'application/x-www-form-urlencoded'
+      },
+      data: formData
+    })
+    .then(res => {
+      // console.log(res);
+
+      this.setState({ imgPreview: res.data.secure_url });
+    })
+    .catch(err => {
+      console.error(err);
+    });
+  }
+
+
   render() {
     return (
       <div className="App">
@@ -21,13 +87,21 @@ class App extends Component {
           <Switch>
             <Route path='/OutfitEdit' component={OutfitEdit} />
             <Route path='/OutfitList' component={OutfitList} />
-            <Route path='/OutfitUpload' component={OutfitUpload} />
+            <Route path='/OutfitUpload' render={(props) =>
+              (
+                <OutfitUpload
+                  handleSubmit={this.handleSubmit}
+                  handleChange = {this.handleChange}
+                  imgPreview={this.state.imgPreview}
+                  {...props}
+                />
+              )}
+            />
             <Redirect to= '/' />
           </Switch>
         </main>
         <div className="propstest">
           <OutfitList / >
-          <OutfitUpload / >
         </div>
       </div>
     );
--- outfit/client/src/components/OutfitUpload.js
+++ outfit/client/src/components/OutfitUpload.js
@@ -1,18 +1,20 @@
 import React, { Component } from 'react';
 
-
 class OutfitUpload extends Component {
-
-
   render() {
-    console.log('');
     return (
       <div>
         <h2>Hello from Outfit Upload page</h2>
-
+      <form onSubmit= {this.props.handleSubmit.bind(this)}>
+      <label className="file-upload-container" htmlFor="file-upload">
+        <input onChange={this.props.handleChange} style= {{ display: 'none' }} id="file-upload" type="file" />
+        <p><img src={this.props.imgPreview === null ? 'http://fillmurray.com/g/300/300' : this.props.imgPreview} alt ="Bill" id="img-preview" /></p>
+      </label><br/>
+      <button type="submit">Submit</button>
+      </form>
       </div>
     )
   }
 }
-export default OutfitUpload;
 
+export default OutfitUpload;
