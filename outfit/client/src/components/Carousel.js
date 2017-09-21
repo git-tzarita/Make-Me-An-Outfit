976fb5ce82607465992d4084e2d768d3763a86df
mao hung <maohung71@gmail.com>
2017-09-21 16:25:56 -0400

    workd on pulling api data

---
+++ outfit/client/src/components/Carousel.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+var ReactDOM = require('react-dom');
+var Carousel = require('react-responsive-carousel').Carousel;
+
+
+class Carousel extends Component {
+  render() {
+        return (
+            <Carousel axis="horizontal|vertical" showThumbs={true|false} showArrows={true|false} onChange={onChange} onClickItem={onClickItem} onClickThumb={onClickThumb} dynamicHeight emulateTouch>
+                <div>
+                    <img src="assets/1.jpeg" />
+                    <p className="legend">Legend 1</p>
+                </div>
+                <div>
+                    <img src="assets/2.jpeg" />
+                    <p className="legend">Legend 2</p>
+                </div>
+                <div>
+                    <img src="assets/3.jpeg" />
+                    <p className="legend">Legend 3</p>
+                </div>
+            </Carousel>
+        );
+    }
+}
+
+
+
+ReactDOM.render(<DemoCarousel />, document.querySelector('.demo-carousel'));
+
+
+// export default Carousel;
--- outfit/client/src/components/OutfitEdit.js
+++ outfit/client/src/components/OutfitEdit.js
@@ -1,6 +1,34 @@
 import React, { Component } from 'react';
+import { Image } from 'cloudinary-react';
+import axios from 'axios';
 
 class OutfitEdit extends Component {
+  constructor(){
+    super();
+    this.state = {
+      apiData: [],
+    }
+    this.handleOnClick = this.handleOnClick.bind(this)
+  }
+
+
+    componentDidMount() {
+      console.log('APP did mount');
+    }
+
+     handleOnClick(){
+      console.log('hello')
+
+        axios("https://api.cloudinary.com/v1_1/ga-mao/resources/image")
+
+          .then(res => {
+            this.setState({
+              apiData: res.data.data.url,
+            })
+            console.log(res.data)
+          })
+      }
+
 
 
 
@@ -17,4 +45,3 @@ class OutfitEdit extends Component {
 
 export default OutfitEdit;
 
-//<img src={this.state.apiData}/>
